5eff87772b04657447dd09fce7036288dca22c52
tqk2811 <tqk2811@gmail.com>
2025-11-27 15:48:08 +0700

    update ImageSourceAttribute

--- TqkLibrary.Avalonia.Utils/Attributes/ImageSourceAttribute.cs
+++ TqkLibrary.Avalonia.Utils/Attributes/ImageSourceAttribute.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Media;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using Avalonia.Svg.Skia;
@@ -8,27 +10,42 @@ namespace TqkLibrary.Avalonia.Utils.Attributes
     [AttributeUsage(AttributeTargets.Field)]
     public class ImageSourceAttribute : Attribute
     {
-        readonly string _path;
+        readonly string _path_or_key;
         /// <summary>
         /// null is auto detech extension
         /// </summary>
         public bool? IsSVG { get; set; } = null;
 
         /// <summary>
-        /// Example: avares://MyApp/Assets/new.png or /Assets/file.svg (load from WorkingDir)
+        /// Key (priority): search in <see cref="ResourceDictionary"/> <br/>
+        /// Path: Example 'avares://MyApp/Assets/file.png' or '/Assets/file.svg' (load from WorkingDir)
         /// </summary>
-        /// <param name="path">Example: avares://MyApp/Assets/new.png</param>
-        public ImageSourceAttribute(string path)
+        public ImageSourceAttribute(string key_or_path)
         {
-            this._path = path;
+            this._path_or_key = key_or_path;
         }
 
         public IImage? GetImage()
         {
-            Stream stream = _path.StartsWith("avares://", StringComparison.OrdinalIgnoreCase)
-                ? AssetLoader.Open(new Uri(_path))
-                : File.OpenRead(_path);
-            if ((IsSVG is null && _path.EndsWith(".svg", StringComparison.OrdinalIgnoreCase)) || IsSVG == true)
+            if (Application.Current?.TryGetResource(_path_or_key, null, out var res) == true)
+            {
+                if (res is IImage img)
+                    return img;
+
+                if (res is string s)
+                {
+                    return LoadFromPath(s, IsSVG);
+                }
+            }
+            return LoadFromPath(_path_or_key, IsSVG);
+        }
+
+        static IImage LoadFromPath(string path, bool? isSvg = null)
+        {
+            Stream stream = path.StartsWith("avares://", StringComparison.OrdinalIgnoreCase)
+            ? AssetLoader.Open(new Uri(path))
+            : File.OpenRead(path);
+            if ((isSvg is null && path.EndsWith(".svg", StringComparison.OrdinalIgnoreCase)) || isSvg == true)
             {
                 return new SvgImage
                 {
