49dfe99e118a60906bf3afabaec1837a6d63ac62
tqk2811 <tqk2811@gmail.com>
2025-11-27 14:48:17 +0700

    update MenuSpacerModel

--- TqkLibrary.Avalonia.Utils/ViewModels/MenuSpacerModel.cs
+++ TqkLibrary.Avalonia.Utils/ViewModels/MenuSpacerModel.cs
@@ -1,9 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TqkLibrary.Avalonia.Utils.ViewModels
 {
-    public class MenuSpacerModel : ObservableObject
+    public class MenuSpacerModel : MenuViewModelBase
     {
-
+        [SetsRequiredMembers]
+        public MenuSpacerModel()
+        {
+            this.DisplayName = "-";
+        }
     }
 }
