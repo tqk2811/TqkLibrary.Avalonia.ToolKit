1cc6b955054dadb2a26c3a2695d6b740720e2eee
tqk2811 <tqk2811@gmail.com>
2025-11-27 17:56:47 +0700

    add ViewModelBase<TData>

--- TqkLibrary.Avalonia.Utils/ViewModels/ViewModelBase.cs
+++ TqkLibrary.Avalonia.Utils/ViewModels/ViewModelBase.cs
@@ -1,17 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections;
 using System.ComponentModel;
+using TqkLibrary.Avalonia.Utils.Interfaces;
 
 namespace TqkLibrary.Avalonia.Utils.ViewModels
 {
     public abstract class ViewModelBase : ObservableObject, INotifyDataErrorInfo
     {
         #region INotifyDataErrorInfo
-        public bool HasErrors => _errors.Any();
+        public virtual bool HasErrors => _errors.Any();
 
-        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+        public virtual event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
 
-        public IEnumerable GetErrors(string? propertyName)
+        public virtual IEnumerable GetErrors(string? propertyName)
         {
             return propertyName != null && _errors.TryGetValue(propertyName, out var list)
                 ? list
@@ -20,22 +21,22 @@ namespace TqkLibrary.Avalonia.Utils.ViewModels
         #endregion
 
         private readonly Dictionary<string, List<string>> _errors = new();
-        protected void AddError(string prop, string error)
+        protected virtual void AddError(string prop, string error)
         {
             _errors[prop] = new List<string>() { error };
             ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(prop));
         }
-        protected void AddError(string prop, IEnumerable<string> errors)
+        protected virtual void AddError(string prop, IEnumerable<string> errors)
         {
             _errors[prop] = [.. errors];
             ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(prop));
         }
-        protected void ClearErrors(string prop)
+        protected virtual void ClearErrors(string prop)
         {
             if (_errors.Remove(prop))
                 ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(prop));
         }
-        protected void ClearErrors()
+        protected virtual void ClearErrors()
         {
             var keys = _errors.Keys.ToList();
             _errors.Clear();
@@ -43,6 +44,33 @@ namespace TqkLibrary.Avalonia.Utils.ViewModels
             {
                 ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(key));
             }
+        }
+    }
+    public abstract class ViewModelBase<TData> : ViewModelBase, IViewModelUpdate<TData>
+    {
+        readonly Action? _saveCallback;
+        TData _data;
+        public event ChangeCallBack<TData>? Change;
+        public virtual TData Data { get { return _data; } }
+
+        public ViewModelBase(TData data)
+        {
+            _data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+        public ViewModelBase(TData data, Action saveCallback) : this(data)
+        {
+            this._saveCallback = saveCallback ?? throw new ArgumentNullException(nameof(saveCallback));
+        }
+
+        public virtual void Save()
+        {
+            _saveCallback?.Invoke();
+            Change?.Invoke(this, Data);
+        }
+
+        public virtual void Update(TData data)
+        {
+            this._data = data ?? throw new ArgumentNullException(nameof(data));
         }
     }
 }
