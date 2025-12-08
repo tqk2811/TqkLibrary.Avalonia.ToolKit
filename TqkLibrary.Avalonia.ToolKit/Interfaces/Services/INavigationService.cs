e98d29dc8ca11fb51e36f3c54e735e88b3cee508
tqk2811 <tqk2811@gmail.com>
2025-12-08 16:36:11 +0700

    update INavigationService

--- TqkLibrary.Avalonia.ToolKit/Interfaces/Services/INavigationService.cs
+++ TqkLibrary.Avalonia.ToolKit/Interfaces/Services/INavigationService.cs
@@ -10,7 +10,12 @@ namespace TqkLibrary.Avalonia.ToolKit.Interfaces.Services
     public interface INavigationService<TBaseViewModel>
     {
         TBaseViewModel? CurrentView { get; }
+        bool IsBackAvalable { get; }
+        bool IsNextAvalable { get; }
         void NavigateTo<TViewModel>() where TViewModel : TBaseViewModel;
+        void NavigateTo(TBaseViewModel viewModel);
         void NavigateTo(Type type);
+        bool Back();
+        bool Next();
     }
 }
